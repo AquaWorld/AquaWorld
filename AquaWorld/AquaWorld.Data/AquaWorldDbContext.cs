3e4f50faa4c6f2a49981a0db426db4caa36b7146
iliangogov <iliangogov@gmail.com>
2017-03-04 19:33:32 +0200

    NinjectWeb.cs and NinjectWebCommon.cs added

--- AquaWorld/AquaWorld.Data/AquaWorldDbContext.cs
+++ AquaWorld/AquaWorld.Data/AquaWorldDbContext.cs
@@ -7,10 +7,11 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
 using AquaWorld.Data.Models;
+using AquaWorld.Data.Contracts;
 
 namespace AquaWorld.Data
 {
-    public class AquaWorldDbContext : IdentityDbContext<User>
+    public class AquaWorldDbContext : IdentityDbContext<User>, IAquaWorldDbContext
     {
         public AquaWorldDbContext()
             : base("DefaultConnection")
