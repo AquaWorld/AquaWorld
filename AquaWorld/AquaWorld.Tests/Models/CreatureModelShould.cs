4f0ef1914db708df446c112d6cc4e9c2e509a570
iliangogov <iliangogov@gmail.com>
2017-03-18 14:10:15 +0200

    CreatureModel tested

---
+++ AquaWorld/AquaWorld.Tests/Models/CreatureModelShould.cs
@@ -0,0 +1,42 @@
+﻿using AquaWorld.Data.Models;
+using AquaWorld.Data.Models.Contracts;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaWorld.Tests.Models
+{
+    [TestFixture]
+    public class CreatureModelShould
+    {
+        [Test]
+        public void SetCorrectlyAllPropertiesWhenInitialized()
+        {
+            //Arrange & Act
+            var actualCreature = new Creature()
+            {
+                Id = 1,
+                Name = "name",
+                Description = "desc",
+                AvailableCount = 2,
+                Category = "fresh",
+                ImageUrl = "http://site.com"
+            };
+
+            //Assert
+            Assert.That(
+                () => new Creature(),
+                Is.InstanceOf<ICreature>());
+
+            Assert.AreEqual(actualCreature.Id, 1);
+            Assert.AreEqual(actualCreature.AvailableCount, 2);
+            Assert.AreEqual(actualCreature.Name, "name");
+            Assert.AreEqual(actualCreature.Description, "desc");
+            Assert.AreEqual(actualCreature.Category, "fresh");
+            Assert.AreEqual(actualCreature.ImageUrl, "http://site.com");
+        }
+    }
+}
