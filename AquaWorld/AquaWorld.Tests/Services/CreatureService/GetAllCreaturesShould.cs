4d764d753f12ff3890675f11e64d14ed03c9232d
iliangogov <iliangogov@gmail.com>
2017-03-18 16:36:56 +0200

    GetAllCreatures method tested

---
+++ AquaWorld/AquaWorld.Tests/Services/CreatureService/GetAllCreaturesShould.cs
@@ -0,0 +1,50 @@
+﻿using AquaWorld.Data.Contracts;
+using AquaWorld.Data.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaWorld.Tests.Services.CreatureService
+{
+    [TestFixture]
+    public class GetAllCreaturesShould
+    {
+        [Test]
+        public void CallCreatureDataProviderAllMethod()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+
+            //Act
+            var actualCreatureService =
+                new AquaWorld.Data.Services.CreatureService(mockedDataProvider.Object);
+
+            actualCreatureService.GetAllCreatures();
+
+            //Assert
+            mockedDataProvider.Verify(
+                service => service.All(),
+                Times.Once);
+        }
+
+        [Test]
+        public void ReturnIqueryableCollectionFromCreatureDataProviderAllMethod()
+        {
+            //Arrange
+            var mockedDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+
+            //Act
+            var actualCreatureService =
+                new AquaWorld.Data.Services.CreatureService(mockedDataProvider.Object);
+
+            var actualResult = actualCreatureService.GetAllCreatures();
+
+            //Assert
+            Assert.That(actualResult, Is.Not.Null.And.InstanceOf<IQueryable<Creature>>());
+        }
+    }
+}
