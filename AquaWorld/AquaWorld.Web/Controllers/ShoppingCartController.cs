244283d97e2ff2610ce012fcefe7c1f121d10c70
iliangogov <iliangogov@gmail.com>
2017-03-19 10:41:13 +0200

    Shopping Cart Controller, navigation link icon and View introduced

---
+++ AquaWorld/AquaWorld.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AquaWorld.Web.Controllers
+{
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
