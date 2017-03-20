a9ebf285e03d600aa75cac43234a4386c70190d8
iliangogov <iliangogov@gmail.com>
2017-03-20 23:35:55 +0200

    OrderService/Constructor tested

---
+++ AquaWorld/AquaWorld.Tests/Services/OrderService/ConstructorShould.cs
@@ -0,0 +1,34 @@
+﻿using AquaWorld.Data.Contracts;
+using AquaWorld.Data.Models;
+using AquaWorld.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaWorld.Tests.Services.OrderService
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void InitializeNewInstanceOfIOrderServiceWhenProperParameterIsPassed()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfAquaWorldDataProvider<Order>>();
+            var mockedCreatureDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+            var mockedOrderToCreate = new Mock<Order>();
+
+            //Act & Assert
+            Assert.That(
+                () => new AquaWorld.Data.Services.OrderService(
+                    mockedOrderDataProvider.Object,
+                    mockedCreatureDataProvider.Object,
+                    mockedOrderToCreate.Object),
+                Is.InstanceOf<IOrderService>());
+        }
+    }
+}
