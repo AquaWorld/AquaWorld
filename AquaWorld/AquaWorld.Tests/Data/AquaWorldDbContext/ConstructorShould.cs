b61db56637715b4416f1f37c74dfdb81a49135d0
iliangogov <iliangogov@gmail.com>
2017-03-21 22:13:35 +0200

    AquaWorldDbContext tested

---
+++ AquaWorld/AquaWorld.Tests/Data/AquaWorldDbContext/ConstructorShould.cs
@@ -0,0 +1,29 @@
+﻿using AquaWorld.Data.Contracts;
+using NUnit.Framework;
+
+namespace AquaWorld.Tests.Data.AquaWorldDbContext
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void HaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var context = new AquaWorld.Data.AquaWorldDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<AquaWorld.Data.AquaWorldDbContext>(context);
+        }
+
+        [Test]
+        public void Return_InstanceOfIPetsWonderlandDbContext()
+        {
+            // Arrange & Act
+            var context = new AquaWorld.Data.AquaWorldDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<IAquaWorldDbContext>(context);
+        }
+    }
+}
---
+++ AquaWorld/AquaWorld.Tests/Data/AquaWorldDbContext/CreateShould.cs
@@ -0,0 +1,20 @@
+﻿using AquaWorld.Data.Contracts;
+using NUnit.Framework;
+
+namespace AquaWorld.Tests.Data.AquaWorldDbContext
+{
+    [TestFixture]
+    public class CreateShould
+    {
+        [Test]
+        public void ReturnNewDbContextInstance()
+        {
+            // Arrange & Act
+            var newContext = AquaWorld.Data.AquaWorldDbContext.Create();
+
+            // Assert
+            Assert.IsNotNull(newContext);
+            Assert.IsInstanceOf<IAquaWorldDbContext>(newContext);
+        }
+    }
+}
