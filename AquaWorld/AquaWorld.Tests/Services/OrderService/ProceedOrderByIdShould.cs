4c60911390e00319e0042196b11733f50fe74c92
iliangogov <iliangogov@gmail.com>
2017-03-23 10:09:30 +0200

    OrderService/ProceedOrderById method tested

---
+++ AquaWorld/AquaWorld.Tests/Services/OrderService/ProceedOrderByIdShould.cs
@@ -0,0 +1,88 @@
+﻿using AquaWorld.Data.Contracts;
+using AquaWorld.Data.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaWorld.Tests.Services.OrderService
+{
+    [TestFixture]
+    public class ProceedOrderByIdShould
+    {
+        [Test]
+        public void CallOrderDataProviderGetByIdMethod()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfAquaWorldDataProvider<Order>>();
+            var mockedCreatureDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new AquaWorld.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedCreatureDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            mockedOrderDataProvider.Verify(x => x.GetById(orderId), Times.Once);
+        }
+
+        [Test]
+        public void CallOrderDataProviderSaveChangesMethod()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfAquaWorldDataProvider<Order>>();
+            var mockedCreatureDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new AquaWorld.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedCreatureDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            mockedOrderDataProvider.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void SetOrderToUpdateIsProceededValueToTrue()
+        {
+            //Arrange
+            var mockedOrderDataProvider = new Mock<IEfAquaWorldDataProvider<Order>>();
+            var mockedCreatureDataProvider = new Mock<IEfAquaWorldDataProvider<Creature>>();
+            var orderId = 5;
+            var mockedOrderToUpdate = new Order() { Id = orderId, isProceeded = false };
+
+
+            mockedOrderDataProvider.Setup(x => x.GetById(orderId)).Returns(mockedOrderToUpdate);
+
+            //Act
+            var actualOrderService = new AquaWorld.Data.Services.OrderService(
+                mockedOrderDataProvider.Object,
+                mockedCreatureDataProvider.Object,
+                mockedOrderToUpdate);
+
+            actualOrderService.ProceedOrderById(orderId);
+
+            //Assert
+            Assert.AreEqual(mockedOrderToUpdate.isProceeded, true);
+        }
+    }
+}
