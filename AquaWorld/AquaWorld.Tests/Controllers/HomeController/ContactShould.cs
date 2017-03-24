5bd51b673f6569582ae609e3adeac613d553057c
iliangogov <iliangogov@gmail.com>
2017-03-24 23:45:21 +0200

    AquaWorld/AquaWorld.Tests/Controllers/HomeController/Contact actionResult tested

---
+++ AquaWorld/AquaWorld.Tests/Controllers/HomeController/ContactShould.cs
@@ -0,0 +1,35 @@
+﻿using AquaWorld.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace AquaWorld.Tests.Controllers.HomeController
+{
+    [TestFixture]
+    public class ContactShould
+    {
+        [Test]
+        public void ReturnViewWithListOfCorrectModelsWhenThereAreAnyExisting()
+        {
+            // Arrange
+            var creatureServiceMock = new Mock<ICreatureService>();
+
+            // Act
+            var homeController = new AquaWorld.Web.Controllers.HomeController(creatureServiceMock.Object);
+            var viewResult = homeController.Contact() as ViewResult;
+
+            //Assert
+            homeController
+                .WithCallTo(c => c.Contact())
+                .ShouldRenderDefaultView();
+
+            Assert.AreEqual("Your contact page.", viewResult.ViewData["Message"]);
+        }
+    }
+}
