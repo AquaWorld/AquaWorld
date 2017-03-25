360737b7bb6b86cfe2068d34c5f7f2070b03274d
iliangogov <iliangogov@gmail.com>
2017-03-25 08:44:18 +0200

    CreaturesController/Index actionView tested

---
+++ AquaWorld/AquaWorld.Tests/Controllers/CreaturesController/IndexShould.cs
@@ -0,0 +1,36 @@
+﻿using AquaWorld.Data.Models;
+using AquaWorld.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TestStack.FluentMVCTesting;
+
+namespace AquaWorld.Tests.Controllers.CreaturesController
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void ReturnViewWithListOfCorrectModelsWhenThereAreAnyExisting()
+        {
+            // Arrange
+            var creatureServiceMock = new Mock<ICreatureService>();
+            Creature firstCreature = new Creature() { Name = "GoldFish" };
+            Creature secondCreature = new Creature() { Name = "ClownFish" };
+
+            var listOfCreatures = new List<Creature>() { firstCreature, secondCreature };
+
+            // Act
+            var creatureController = new AquaWorld.Web.Controllers.CreaturesController(creatureServiceMock.Object);
+
+            //Assert
+            creatureController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView()
+                .WithModel<IList<Creature>>(model =>
+                {
+                    Assert.IsInstanceOf(typeof(IList<Creature>), model);
+                });
+        }
+    }
+}
