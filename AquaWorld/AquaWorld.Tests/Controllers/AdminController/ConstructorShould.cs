9d9f31a31228334607e9dbc8b1e0b05beef7f6e6
iliangogov <iliangogov@gmail.com>
2017-03-26 10:24:30 +0300

    AdminController/Constructor tested

---
+++ AquaWorld/AquaWorld.Tests/Controllers/AdminController/ConstructorShould.cs
@@ -0,0 +1,56 @@
+﻿using AquaWorld.Data.Services;
+using AquaWorld.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace AquaWorld.Tests.Controllers.AdminController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ReturnsAnInstanceWhenParameterIsNotNull()
+        {
+            // Arrange
+            var creatureServiceMock = new Mock<ICreatureService>();
+            var orderServiceMock = new Mock<IOrderService>();
+
+            // Act
+            var adminController = new AquaWorld.Web.Areas.Admin.AdminController(
+                creatureServiceMock.Object, orderServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(adminController);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AquaWorld.Web.Areas.Admin.AdminController(null, null));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenCreatureServiceIsNull()
+        {
+            // Arrange
+            CreatureService creatureServiceMock = null;
+            var orderServiceMock = new Mock<IOrderService>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AquaWorld.Web.Areas.Admin.AdminController(creatureServiceMock, orderServiceMock.Object));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenOrderServiceIsNull()
+        {
+            // Arrange
+            var creatureServiceMock = new Mock<ICreatureService>();
+            OrderService orderServiceMock = null;
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AquaWorld.Web.Areas.Admin.AdminController(creatureServiceMock.Object, orderServiceMock));
+        }
+    }
+}
