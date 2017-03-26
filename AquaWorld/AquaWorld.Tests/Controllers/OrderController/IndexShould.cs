fa2bf9217e300a61dff26cdb4b8f78a28a9a7ea8
iliangogov <iliangogov@gmail.com>
2017-03-26 14:42:15 +0300

    OrderController/MyOrders tested

---
+++ AquaWorld/AquaWorld.Tests/Controllers/OrderController/IndexShould.cs
@@ -0,0 +1,26 @@
+﻿using AquaWorld.Data.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace AquaWorld.Tests.Controllers.OrderController
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void ReturnView()
+        {
+            // Arrange
+            var orderServiceMock = new Mock<IOrderService>();
+
+            // Act
+            var creatureController = new AquaWorld.Web.Controllers.OrderController(orderServiceMock.Object);
+
+            //Assert
+            creatureController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
---
+++ AquaWorld/AquaWorld.Tests/Controllers/OrderController/MyOrdersShould.cs
@@ -0,0 +1,44 @@
+﻿using AquaWorld.Data.Models;
+using AquaWorld.Data.Services.Contracts;
+using AquaWorld.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TestStack.FluentMVCTesting;
+
+namespace AquaWorld.Tests.Controllers.OrderController
+{
+    [TestFixture]
+    class MyOrdersShould
+    {
+        [Test]
+        public void ReturnViewWithCurrentUserOrdersList()
+        {
+            // Arrange
+            var orderServiceMock = new Mock<IOrderService>();
+            var currentUser = new User() { Id = "id" };
+            var firstOrder = new Order() { UserId = "id" };
+            var secondOrder = new Order() { UserId = "notMachedId" };
+            var listOfOrders = new List<Order>()
+            {
+                firstOrder,
+                secondOrder
+            };
+
+            orderServiceMock.Setup(x => x.GetOrdersByUserId("id")).Returns(listOfOrders.AsQueryable);
+
+            // Act
+            var orderController = new AquaWorld.Web.Controllers.OrderController(orderServiceMock.Object);
+
+            //Assert
+            orderController
+                .WithCallTo(c => c.MyOrders("id"))
+                .ShouldRenderDefaultView()
+                .WithModel<List<OrderViewModel>>(model =>
+                {
+                    Assert.AreEqual(model.First().UserId, currentUser.Id);
+                });
+        }
+    }
+}
